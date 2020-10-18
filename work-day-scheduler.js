761ad045e501c6a7bd21d8eb4aa5fb1b6ce4d3e0
G Dog <greg.hanstedt@med.ge.com>
2020-10-18 11:02:37 -0400

    Reasonably finished version - but needs proper save icon.

--- work-day-scheduler.js
+++ work-day-scheduler.js
@@ -1,14 +1,14 @@
+const standardDayBegin = 7;
+const standardDayEnd = 18;
+const textAreaColumns = 90;
+const textAreaRows = 4;
+var currentHour = 11;
+var hourSelected;
+var hoursNoteString;
+var calendarDayText = [];
+var currentDate;
+
 $(document).ready(function() {
-
-    const standardDayBegin = 7;
-    const standardDayEnd = 18;
-    const textAreaColumns = 90;
-    const textAreaRows = 4;
-    var currentHour = 11;
-    var hourSelected;
-    var hoursNoteString;
-    var calendarDayText = [];
-
 
     function emptySchedule () {
         $(".container").empty ();
@@ -20,6 +20,7 @@ $(document).ready(function() {
         var textAreaElement;
         var buttonElement;
         var timePeriodText;
+        var timeText;
         var counter;
         for (counter = standardDayBegin; counter <= standardDayEnd; counter ++) {
 
@@ -61,9 +62,91 @@ $(document).ready(function() {
         }
     }
 
-    emptySchedule ();
-    buildSchedule ();
+    function writeCalendarData () {
+        var workingTextArea;
 
+        if (calendarDayText.length > 0) {
+            for (counter = 0; counter < calendarDayText.length; counter ++) {
+                if (calendarDayText [counter] !== null) {
+                    console.log ("Updating hour: " + counter);
+                    workingTextArea = "textArea" + counter;
+                    $(`#${workingTextArea}`).val (calendarDayText [counter]);
+                }
+            }
+        }
+
+        else {
+            console.log ("There is no existig data");
+        }
+    }
+
+    function loadCalendarData () {
+        var tempArray = JSON.parse(localStorage.getItem("GDOG-Work-Day-Scheduler"));
+        if (tempArray !== null) {
+          calendarDayText = tempArray;
+        }
+    }
+      
+    function saveCalendarData () {
+        localStorage.setItem ("GDOG-Work-Day-Scheduler", JSON.stringify(calendarDayText));
+    }
+
+    function getMonthText (currentDate) {
+        var month = [];
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+        return month[currentDate.getMonth()];        
+    }
+
+    function getDayText (currentDate) {
+        var days = [];
+        days[0] = "Sunday";
+        days[1] = "Monday";
+        days[2] = "Tuesday";
+        days[3] = "Wednesday";
+        days[4] = "Thursday";
+        days[5] = "Friday";
+        days[6] = "Saturday";
+        return days[currentDate.getDay()];        
+    }
+
+    function writeCurrentDate (currentDate) {
+        var fullDateText;
+        var monthText = getMonthText (currentDate);
+        var date = currentDate.getDate ();
+        var dayText = getDayText (currentDate);
+        var year = currentDate.getFullYear ();
+
+        fullDateText = `${dayText}, ${monthText} ${date}, ${year}`;
+
+        $("#currentDay").text (fullDateText);
+
+        console.log (`Date:  ${fullDateText}`);
+    }
+
+    var d = new Date("July 21, 1983 01:15:00");
+    var n = d.getDate();
+    console.log ("N: "+ n);
+
+    currentDate = new Date ();
+    currentHour = currentDate.getHours();
+    console.log (currentDate.getMonth ());
+    console.log (currentDate);
+    emptySchedule ();
+    loadCalendarData ();
+    writeCurrentDate (currentDate);
+    buildSchedule ();
+    writeCalendarData ();
     
     $(".saveBtn").on ("click", function (event) {
         event.preventDefault ();
@@ -73,8 +156,10 @@ $(document).ready(function() {
         console.log ("Selected text area: " + selectedTextArea);
         hoursNoteString = $(`#${selectedTextArea}`).val ();
         calendarDayText [parseInt (hourSelected)] = hoursNoteString;
+        saveCalendarData ();
 
         console.log ("Hours note:  " + calendarDayText);
+
     });
 });
 
